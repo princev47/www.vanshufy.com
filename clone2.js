dfb5eaa6f426061033531130ef63486a97cb656f
Harsh-Pachauri <151666028+Harsh-Pachauri@users.noreply.github.com>
2024-07-24 17:42:04 +0530

    Update clone2.js

--- clone2.js
+++ clone2.js
@@ -81,7 +81,7 @@ const playMusic=(track,pause=false)=>{
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/www.musified.com/songsClone2/`)
+    let a = await fetch(`/songsClone2/`)
     let response = await a.text();
     // console.log(response)
     let div = document.createElement("div")
