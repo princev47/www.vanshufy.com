921a7785e5b727727386481ebd439f49faad2815
Harsh-Pachauri <151666028+Harsh-Pachauri@users.noreply.github.com>
2024-07-25 06:59:47 +0530

    Update clone2.js

--- clone2.js
+++ clone2.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds){
 }
 async function getSongs(folder){
     currFolder = folder;
-    let a=await fetch(`https://github.com/Harsh-Pachauri/www.musified.com/tree/main/${folder}/`);
+    let a=getDirectory(`/${folder}/`);
     // console.log(a.text())
     let response=await a.text();
     // console.log(response)
